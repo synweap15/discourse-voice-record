35332362397baecce5bd7039e4fc56287715af5a
Angus McLeod <angus@mcleod.org.au>
2020-04-24 08:48:16 +1000

    Add microphone svg

--- plugin.rb
+++ plugin.rb
@@ -7,6 +7,8 @@ enabled_site_setting :composer_audio_upload_enabled
 
 register_asset 'stylesheets/audio-upload.scss'
 
+register_svg_icon "microphone" if respond_to?(:register_svg_icon)
+
 register_html_builder('server:before-head-close') do
   #'<script src="/plugins/discourse-audio-upload/javascripts/vendor/RecordRTC.min.js"></script>'
   '<script src="/plugins/discourse-audio-upload/javascripts/vendor/microm.min.js"></script>'
